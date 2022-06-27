3bd9b9bd5d976741ed879aca4686100e8a59e9ae
Chang Jing Yan <changjy44@gmail.com>
2022-06-27 13:35:22 +0800

    refactored query to be sent in frontend

--- index.js
+++ index.js
@@ -3,98 +3,38 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const app = express();
 const neo4j = require("neo4j-driver");
+/**
+ * Change the following details when connecting to a new neo4j database.
+ */
 const uri = "neo4j://13.213.80.230:7687";
 const user = "neo4j";
 const password = "8H2er2re6e";
+/*<-----------------------------------------> */
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
-// var json = require("./force.json");
 
 app.use("/api", (req, res) => {
   const driver = neo4j.driver(uri, neo4j.auth.basic(user, password));
   const session = driver.session();
   try {
-    console.log("working!");
     async function getAllNodes() {
       console.log(req.body.name);
       const result = await session
         .readTransaction((tx) => {
-          // return tx.run("MATCH (p {name:'Shaun Wright-Phillips'}) RETURN p;");
-          // const year = Number(req.body.year);
-          // console.log(year);
           const query = req.body.query;
           return tx.run(query);
         })
         .then((data) => {
-          //   console.log(data.records[0]);
-          //   console.log(data.records);
-          // console.log(data.records[0]._fields[0].properties.Name);
-          // console.log(data.records[0]._fields[0].labels[0]); --> For player labels
-          // console.log(
-          //   data.records.map((entry) => {
-          //     return entry._fields[1];
-          //   })
-          // );
-          // console.log(data.records[0]._fields);
-
           if (data.records.length > 0) {
-            const targetPlayer = {
-              id: data.records[0]._fields[0].properties.Name,
-              group: 1,
-              label: data.records[0]._fields[0].labels[0],
-            };
-
-            let clubArray = data.records.map((entry) => {
-              const json = {
-                id: entry._fields[2].properties.clubName,
-                group: 3,
-                label: entry._fields[0].labels[0],
-              };
-              return json;
-            });
-
-            clubArray = clubArray.filter((entry, index) => {
-              for (let i = 0; i < index; i++) {
-                if (clubArray[i].id === clubArray[index].id) {
-                  return false;
-                }
-              }
-              return true;
-            });
-
-            let playerNodes = data.records.map((entry) => {
-              const json = {
-                id: entry._fields[4].properties.Name,
-                group: 2,
-                label: entry._fields[0].labels[0],
-              };
-              return json;
-            });
-
-            playerNodes = playerNodes.filter((entry, index) => {
-              for (let i = 0; i < index; i++) {
-                if (playerNodes[i].id === playerNodes[index].id) {
-                  return false;
-                }
-              }
-              return true;
-            });
-
-            clubArray.push(targetPlayer);
-            clubArray = clubArray.concat(playerNodes);
-
-            // nodesArray.push(targetClub);
-
-            // const targetPlayerLink = {
-            //   source: data.records[0]._fields[0].properties.Name,
-            //   target: data.records[0]._fields[2].properties.clubName,
-            //   value: 1,
-            // };
             let finalNodes = [];
             let finalPlayerLinks = [];
 
+            /**
+             * This function checks whether the entry is a node or link, and returns its name
+             * @param {*} columnEntry
+             * @returns name of entry
+             */
             function getLabelOrRelation(columnEntry) {
               if (columnEntry.labels) {
                 return columnEntry.labels[0];
@@ -103,7 +43,28 @@ app.use("/api", (req, res) => {
               }
             }
 
+            /**
+             * This function returns an array of unique player nodes.
+             * @param {*} nodesArray
+             * @returns An array of unique player nodes.
+             */
+
             function filterNodes(nodesArray) {
+              // As long as any node has group 1, it sets all the nodes  with the same name as group 1.
+              nodesArray.forEach((entry, index) => {
+                for (let i = 0; i < index; i++) {
+                  if (nodesArray[i].id === nodesArray[index].id) {
+                    if (
+                      nodesArray[i].group === 1 ||
+                      nodesArray[index].group === 1
+                    ) {
+                      nodesArray[i].group = 1;
+                      nodesArray[index].group = 1;
+                    }
+                  }
+                }
+              });
+
               nodesArray = nodesArray.filter((entry, index) => {
                 for (let i = 0; i < index; i++) {
                   if (nodesArray[i].id === nodesArray[index].id) {
@@ -115,6 +76,11 @@ app.use("/api", (req, res) => {
               return nodesArray;
             }
 
+            /**
+             * This function returns an array of unique links
+             * @param {*} linksArray
+             * @returns An array of unique links
+             */
             function filterLinks(linksArray) {
               linksArray = linksArray.filter((entry, index) => {
                 for (let i = 0; i < index; i++) {
@@ -131,12 +97,23 @@ app.use("/api", (req, res) => {
               return linksArray;
             }
 
+            /**
+             * This function modifies the instanceNumber and unique attributes of the links
+             * instanceNumber counts the number of links that start from the same person, and gives them an index.
+             * unique counts the number of links that start from person X to person Y, and gives them an index. These links will
+             * be curved in the frontend,.
+             *
+             * @param {*} linksArray
+             * @returns the modified links array
+             */
             function editInstanceNumber(linksArray) {
               linksArray.forEach((entry, index) => {
                 for (let i = 0; i < index; i++) {
                   if (linksArray[i].source === linksArray[index].source) {
                     if (linksArray[i].target === linksArray[index].target) {
                       entry.unique++;
+                      entry.curved = true;
+                      linksArray[i].curved = true;
                       // linksArray[i].unique = false;
                     }
 
@@ -147,6 +124,13 @@ app.use("/api", (req, res) => {
               return linksArray;
             }
 
+            /**
+             * The main bulk of processing the data.
+             * All links have the following attributes:
+             * source, target, value, startYear, endYear, instanceNumber, unique, curved
+             * All playes/clubs have the following attributes
+             * id, group, label
+             */
             for (let i = 0; i < data.records.length; i++) {
               const rowEntry = data.records[i]._fields;
               for (let j = 0; j < rowEntry.length; j++) {
@@ -158,8 +142,10 @@ app.use("/api", (req, res) => {
                     target: columnEntry.properties.clubName,
                     value: 1,
                     startYear: columnEntry.properties.start.low,
+                    endYear: columnEntry.properties.end.low,
                     instanceNumber: 1,
                     unique: 1,
+                    curved: false,
                   };
                   finalPlayerLinks.push(json);
                 } else if (label === "Player") {
@@ -191,127 +177,33 @@ app.use("/api", (req, res) => {
 
             finalNodes = filterNodes(finalNodes);
             finalPlayerLinks = filterLinks(finalPlayerLinks);
-            // console.log(finalPlayerLinks[99]);
+            /**
+             * Sorts the array so that the most recent link gets colored red in the frontend.
+             */
             finalPlayerLinks = finalPlayerLinks.sort((a, b) => {
               return a.startYear > b.startYear ? -1 : 1;
             });
-            // console.log(finalPlayerLinks.map((data) => data.startYear));
             finalPlayerLinks = editInstanceNumber(finalPlayerLinks);
-            // console.log(finalPlayerLinks);
-            /*
-            let targetPlayerLinks = data.records.map((entry) => {
-              const json = {
-                source: entry._fields[0].properties.Name,
-                target: entry._fields[2].properties.clubName,
-                value: 1,
-                startYear: entry._fields[1].properties.start.low,
-                instanceNumber: 1,
-                unique: true,
-              };
-              return json;
-            });
 
-            targetPlayerLinks = targetPlayerLinks.filter((entry, index) => {
-              for (let i = 0; i < index; i++) {
-                if (
-                  targetPlayerLinks[i].target ===
-                    targetPlayerLinks[index].target &&
-                  targetPlayerLinks[i].startYear ===
-                    targetPlayerLinks[index].startYear
-                ) {
-                  return false;
-                }
-              }
-              return true;
-            });
-
-            let secondLinksArray = data.records.map((entry) => {
-              const json = {
-                source: entry._fields[4].properties.Name,
-                target: entry._fields[2].properties.clubName,
-                value: 1,
-                startYear: entry._fields[3].properties.start.low,
-                instanceNumber: 1,
-                unique: true,
-              };
-              return json;
-            });
-
-            secondLinksArray = secondLinksArray.filter((entry, index) => {
-              for (let i = 0; i < index; i++) {
-                if (
-                  secondLinksArray[i].source === secondLinksArray[index].source
-                ) {
-                  return false;
-                }
-              }
-              return true;
-            });
-
-            // console.log(secondLinksArray);
-
-            secondLinksArray = secondLinksArray.concat(targetPlayerLinks);
-
-            secondLinksArray.sort((a, b) => {
-              a.props > b.props ? -1 : 1;
-            });
-
-            secondLinksArray.forEach((entry, index) => {
-              for (let i = 0; i < index; i++) {
-                if (
-                  secondLinksArray[i].source ===
-                    secondLinksArray[index].source &&
-                  secondLinksArray[i].target === secondLinksArray[index].target
-                ) {
-                  entry.instanceNumber++;
-                  entry.unique = false;
-                  secondLinksArray[i].unique = false;
-                }
-              }
-            });
-
-            
-            secondLinksArray = secondLinksArray.filter((entry) => {
-              if (!entry.unique) {
-                nonUniqueLinksArray.push(entry);
-                return false;
-              } else {
-                return true;
-              }
-            });*/
-
-            // console.log(secondLinksArray.length);
-
-            // secondLinksArray.push(targetPlayerLink);
-            // console.log(clubArray);
             var json = {
-              // nodes: [],
               nodes: finalNodes,
-
               links: finalPlayerLinks,
-              // nonUniqueLinks: nonUniqueLinksArray,
             };
 
             res.end(JSON.stringify(json));
             return data;
           }
+        })
+        .catch((e) => {
+          console.log(e);
         });
-      //   console.log(result);
       session.close();
       driver.close();
     }
     getAllNodes();
-
-    //   const singleRecord = result.records[0];
-    //   const node = singleRecord.get(0);
-    //   console.log(node.properties.name);
-    // } finally {
-    //   session.close();
   } catch (e) {
     console.log(e);
   }
-
-  //   console.log(req.body);
 });
 
 app.listen(8000, () => {
